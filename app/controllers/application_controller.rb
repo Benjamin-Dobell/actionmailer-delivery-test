ac582dd8b62bc195611e3fce468a23a6957899b1
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2019-02-26 18:15:38 +1100

    Root attempts to send mail (for reproduction without rails console)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def index
+    mail = TestMailer.test_mail
+    mail.deliver
+    render plain: "Delivered using #{mail.delivery_method.class}"
+  end
 end
