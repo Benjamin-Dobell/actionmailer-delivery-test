6bae4a65c4285f6f4627e178eb6aaefea5bdcd22
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2019-02-26 21:42:18 +1100

    Improved output at the root index to demonstrate the fix/failure.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   def index
     mail = TestMailer.test_mail
-    mail.deliver
-    render plain: "Delivered using #{mail.delivery_method.class}"
+
+    begin
+      mail.deliver!
+      render plain: "Delivered using #{mail.delivery_method.class}"
+    rescue => e
+      render plain: "Attempted delivery with #{mail.delivery_method.class}: #{e.message}"
+    end
   end
 end
